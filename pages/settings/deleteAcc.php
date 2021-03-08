b4da7205ba1490f22d03bf44a05e21e03e034b30
erica-fer <ericafer@uw.edu>
2021-03-08 13:24:13 -0800

    basic login implemented

---
+++ pages/settings/deleteAcc.php
@@ -0,0 +1,64 @@
+<html lang="en">
+
+<?php
+
+$pdo = new PDO('mysql:host=localhost;post=3306;dbname=fullplate_users', 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$errors = [];
+
+$email = '';
+$password = '';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (!$password) {
+        $errors[] = 'Please provide a password.';
+    }
+
+    // ? error checks got here
+
+    // execute on task if there are no issues with input
+    if (empty($errors)) {
+    }
+}
+
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <title>Full-Plate</title>
+    <link rel="stylesheet" type="text/css" href="css/materialize.css">
+</head>
+
+<body style="background-color: bisque;">
+<h1>DELETE ACCOUNT: UNDER CONSTRUCTION</h1>
+    <p class="center-align">
+        <a href="../../index.html" class="btn-floating btn-large waves-effect waves-light grey"><i class="material-icons">arrow_back</i></a><br>
+        Return to welcome page
+    </p>
+
+
+    <!--JavaScript at end of body for optimized loading-->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script>
+        $(document).ready(function() {
+
+        });
+    </script>
+
+
+</body>
+
+</html>
