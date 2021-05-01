ffc35dc539cc2d1918d736232fc68368db3bf9fc
erica-fer <ericafer@uw.edu>
2021-04-30 22:55:10 -0400

    summary added loopback call and updated sql request to ignore null data

--- js/summary.js
+++ js/summary.js
@@ -1,32 +1,53 @@
-
 window.addEventListener('load',
     function () {
-        callDatabase();
+        // Use callback methods to pass data from PHP to be processed
+        // Allows for async processing of information
+        callDatabase(combineData);
     }, false);
 
+function returnResult(result){
+    return result;
+}
 
 // Return the database information between the given date numbers
-function callDatabase() {
+// ? TODO: include a call to validate user request in admin
+function callDatabase(callback) {
     out("entered");
+    var myObj;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            myObj = this.responseText;
+            // out(myObj);
+            callback(myObj);
+        }
+    };
+    xhttp.open("POST", "../php/summaryData.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("date1=2021-04-01&date2=2021-04-21"); // ? TODO: need dates to be variables
 }
 
 // ? do we need this
 // Get all data into one easy to sort array
 // Keeps the sortData() function more specific
-function combineData(){}
+function combineData(data) { 
+    out(data);
+    
+}
 
 // Sort the data gotten from the database
 // Should be called before formatting data
 // Makes it easier to know which was most common bad-day food
-function sortData(){}
+function sortData() { }
 
 // Format the data based on most common
 // Should list out top 3
-function formatData(){}
+function formatData() { }
 
 
 /* DEBUG ONLY FUNCTIONS */
 // Easier output messages
-function out(msg){
+// Lets you see the function which called the given message
+function out(msg) {
     console.log(arguments.callee.caller.name + "() msg: " + msg);
 }
--- php/summaryData.php
+++ php/summaryData.php
@@ -2,22 +2,32 @@
 $pdo = new PDO('mysql:host=localhost;post=3306;dbname=fullplate_users', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$date1 = $_GET['date1'];
-$date2 = $_GET['date2'];
+// Get the date information from the URL
+$date1 = $_POST['date1'];
+$date2 = $_POST['date2'];
 
 // ? test values
-$date1 = '2021-04-01';
-$date2 = '2021-04-21';
+// $date1 = '2021-04-01';
+// $date2 = '2021-04-21';
 
+// Select all food values which appeared on a bad day
+// Helps eliminate irrelevant food info
+// ? may need to include okay days as well
 $statement = $pdo->prepare(
     "SELECT breakfast, lunch, dinner
     FROM foodForDay 
     WHERE date >=:date1 
-    AND date <=:date2");
+    AND date <=:date2
+    AND date IN (
+	    SELECT date
+	    FROM foodForDay
+	    WHERE dayQuality = 'b'
+	)
+    AND (breakfast <> '' OR lunch <> '' OR dinner <> '')");
 $statement->bindValue(':date1', $date1);
 $statement->bindValue(':date2', $date2);
 $statement->execute();
 $food = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-echo var_dump($food);
+echo var_dump($food); // ? DEBUG
 ?>
